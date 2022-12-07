05761378feb671d6151c71cb79fc996bfa03b057
Ryan Pratama <ru.ryanpratama@gmail.com>
2022-12-07 17:11:30 +0300

    aduh

--- src/pages/Expense.js
+++ src/pages/Expense.js
@@ -6,15 +6,31 @@ const Expense = () => {
   useEffect(() => {
     document.title = "#5: Expense Tracker App";
   }, []);
-
-  const ruble = "₽";
+  const [expenseName, setExpenseName] = useState("");
+  const [transaction, setTransaction] = useState(Math.round(0));
+  const [balance, setBalance] = useState(0);
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+  const [date, setDate] = useState("");
   const [data, setData] = useState([
     {
-      expenseTitle: 320,
-      currency: "$",
+      expenseName: "Transportation",
+      transaction: 2,
       date: "2022-30-12",
     },
   ]);
+
+  const removeExpense = (index) => {
+    const newData = [...data];
+    newData.splice(index, 1);
+    setData(newData);
+  };
+
+  useEffect(() => {
+    setBalance(balance + transaction);
+  });
+
+  // const sign = transaction.amount < 0 ? "-" : "+";
 
   return (
     <div className="ceo font-mono">
@@ -29,8 +45,94 @@ const Expense = () => {
             </h2>
           </div>
           <section class="flex flex-wrap justify-around items-center w-full">
-            <ExpenseCard />
+            <div>
+              <div class="w-96 mx-2 my-2">
+                <div class="p-4 max-w-md bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                  <h3 class="text-xl font-bold leading-none text-gray-900 dark:text-white mb-4">
+                    BALANCE: ${balance}
+                  </h3>
+                  <div class="flex justify-between items-center mb-4">
+                    <h3 class="text-xl font-bold leading-none text-gray-900 dark:text-white">
+                      History
+                    </h3>
+                  </div>
+                  <div
+                    className="mt-4 border-b-2 mb-2 pb-2 flex flex-wrap justify-around items-center w-full
+"
+                  >
+                    <p>Income</p>
+                    <p>Expense</p>
+                  </div>
+                  <div
+                    className="mt-1 flex flex-wrap justify-around items-center w-full
+"
+                  >
+                    <p>{income}</p>
+                    <p>{expense}</p>
+                  </div>
+                  {data?.map((expense, index) => {
+                    return (
+                      <ExpenseCard
+                        expenseNameProps={expense.expenseName}
+                        dateProps={expense.date}
+                        totalMoneyProps={expense.totalMoney}
+                        transactionProps={expense.transaction}
+                        index={index}
+                        expense={expense}
+                        removeExpense={removeExpense}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
           </section>
+          <div class="flex flex-col justify-center items-center w-full text-left">
+            <InputField
+              fieldProps={"What expense"}
+              valueTyped={(e) => {
+                setExpenseName(e);
+              }}
+              type="text"
+              lengthMax={256}
+              placeholderValue="Housing"
+            />
+
+            <InputField
+              fieldProps={"Amount"}
+              valueTyped={(e) => {
+                setTransaction(e);
+              }}
+              type="number"
+              placeholderValue="250"
+            />
+
+            <InputField
+              fieldProps={"Date"}
+              valueTyped={(e) => {
+                setDate(e);
+              }}
+              type="date"
+            />
+          </div>
+          <div class="flex justify-center items-center mt-6">
+            <button
+              class="btn border-transparent hover:border-transparent text-white font-extrabold rounded-lg text-sm  text-center bg-indigo-700 dark:bg-indigo-700 hover:bg-indigo-900 dark:hover:bg-indigo-900"
+              onClick={() => {
+                if (
+                  expenseName.length > 0 &&
+                  transaction > 0 &&
+                  date.length > 0
+                ) {
+                  setBalance(balance + transaction);
+                  console.log(balance);
+                  setData([...data, { expenseName, transaction, date }]);
+                }
+              }}
+            >
+              ADD TO LIST
+            </button>
+          </div>
         </div>
       </section>
     </div>
--- src/pages/components/ExpenseCard.jsx
+++ src/pages/components/ExpenseCard.jsx
@@ -1,76 +1,34 @@
 import React from "react";
-import ExpenseInput from "./ExpenseInput";
 
-const ExpenseCard = ({
-  placeholderValue,
-  type,
-  valueTyped,
-  lengthMax,
-  fieldProps,
-}) => {
+const ExpenseCard = ({ transactionProps, dateProps, expenseNameProps }) => {
   return (
     <div>
-      <div class="w-96 mx-2 my-2">
-        <div class="p-4 max-w-md bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-          <div class="flex justify-between items-center mb-4">
-            <h3 class="text-xl font-bold leading-none text-gray-900 dark:text-white">
-              Latest spending
-            </h3>
-          </div>
-          <div class="flow-root">
-            <ul
-              role="list"
-              class="divide-y divide-gray-200 dark:divide-gray-700"
-            >
-              <li class="py-3">
-                <div class="flex items-center space-x-4">
-                  <label>
-                    <input type="checkbox" class="checkbox" />
-                  </label>
-                  <div class="flex-shrink-0">
-                    <img
-                      class="w-8 h-8 rounded-full"
-                      src="https://flowbite.com/docs/images/people/profile-picture-1.jpg"
-                      alt="Neil image"
-                    />
-                  </div>
-                  <div class="flex-1 min-w-0">
-                    <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                      Neil Sims
-                    </p>
-                    <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                      email@windster.com
-                    </p>
-                  </div>
-                  <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                    $320
-                  </div>
-                </div>
-              </li>
-            </ul>
-            <div class="flex justify-start items-center mt-2">
-              <button
-                class="btn border-transparent hover:border-transparent text-white font-extrabold rounded-lg text-sm  text-center bg-red-700 dark:bg-red-700 hover:bg-red-900 dark:hover:bg-red-900"
-                onClick={() => {}}
-              >
-                Delete
-              </button>
+      <div class="flow-root">
+        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+          <li class="py-3">
+            <div class="flex items-center space-x-4">
+              <div class="flex-1 min-w-0">
+                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                  {expenseNameProps}
+                </p>
+                <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                  {dateProps}
+                </p>
+              </div>
+              <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                ${transactionProps}
+              </div>
             </div>
-            <ExpenseInput />
-            <ExpenseInput />
-            <ExpenseInput />
-            <ExpenseInput />
-            <ExpenseInput />
-            <div class="flex justify-center items-center mt-6">
-              <button
-                class="btn border-transparent hover:border-transparent text-white font-extrabold rounded-lg text-sm  text-center bg-indigo-700 dark:bg-indigo-700 hover:bg-indigo-900 dark:hover:bg-indigo-900"
-                onClick={() => {}}
-              >
-                ADD TO LIST
-              </button>
-            </div>
-          </div>
-        </div>
+          </li>
+        </ul>
+        {/* <div class="flex justify-start items-center mt-2">
+          <button
+            class="btn w-12 h-12 border-transparent hover:border-transparent text-white font-extrabold rounded-lg text-sm  text-center bg-red-700 dark:bg-red-700 hover:bg-red-900 dark:hover:bg-red-900"
+            onClick={() => {}}
+          >
+            DELETE
+          </button>
+        </div> */}
       </div>
     </div>
   );
