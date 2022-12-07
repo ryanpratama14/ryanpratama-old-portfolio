337b6def10d4d4fff474b6204d590634ec4429a7
Ryan Pratama <ru.ryanpratama@gmail.com>
2022-12-07 13:26:00 +0300

    margin fixed

--- src/pages/ToDoCard.jsx
+++ src/pages/ToDoCard.jsx
@@ -16,7 +16,7 @@ const ToDoCard = ({
   const [word, setWord] = useState("on progress...💪");
 
   return (
-    <div class="mt-2 mb-6 shadow-lg rounded-xl w-96 md:w-96 py-6 px-6 bg-white overflow-hidden">
+    <div class="mt-2 mb-6 shadow-lg rounded-xl w-96 md:w-96 md:mx-6 md:my-6 py-6 px-6 bg-white overflow-hidden">
       <Toaster />
       <div class="flex items-center border-b-2 mb-2 pb-2">
         <img
