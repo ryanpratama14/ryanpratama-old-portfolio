91f97f4e13c288bb666407167b40341610eda6d9
Ryan Pratama <ru.ryanpratama@gmail.com>
2022-12-08 14:42:59 +0300

    yess

--- src/pages/ExpenseApp.js
+++ src/pages/ExpenseApp.js
@@ -3,10 +3,45 @@ import ExpenseCard from "./ExpenseCard";
 import InputField from "./components/InputField";
 import InputFieldInt from "./components/InputFieldInt";
 
-const Expense = () => {
+const Expense = (props) => {
   useEffect(() => {
     document.title = "#5: Expense Tracker App";
   }, []);
+
+  const [amount, setAmount] = useState();
+  const [desc, setDesc] = useState();
+  const [date, setDate] = useState();
+  const [type, setType] = useState("EXPENSE");
+  const [data, setData] = useState([
+    {
+      amount: 2210,
+      desc: "Tiket to Tokyo",
+      date: "2022-30-12",
+      type: "EXPENSE",
+    },
+    {
+      amount: 4990,
+      desc: "Monthly salary",
+      date: "2023-01-12",
+      type: "INCOME",
+    },
+  ]);
+  const [isAddTxnVisible, toggleAddTXn] = useState(false);
+  const [expense, updateExpense] = useState(0);
+  const [income, updateIncome] = useState(0);
+
+  const calculateBalance = () => {
+    let exp = 0;
+    let inc = 0;
+    data.map((payload) =>
+      payload.type === "EXPENSE"
+        ? (exp = exp + payload.amount)
+        : (inc = inc + payload.amount)
+    );
+    updateExpense(exp);
+    updateIncome(inc);
+  };
+  useEffect(() => calculateBalance(), [data]);
 
   return (
     <div className="ceo font-mono">
@@ -26,10 +61,101 @@ const Expense = () => {
                 <div class="p-4 max-w-md bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
                   <div class="flex justify-between items-center">
                     <h3 class="text-xl font-bold leading-none text-gray-900 dark:text-white text-right">
-                      BALANCE HERE: $
+                      BALANCE: ${income - expense}
                     </h3>
-                    <button className="btn btn-primary">ADD</button>
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => toggleAddTXn((isVisible) => !isVisible)}
+                    >
+                      {isAddTxnVisible ? "CANCEL" : "ADD"}
+                    </button>
                   </div>
+                  {isAddTxnVisible && (
+                    <div
+                      isAddTxnVisible={isAddTxnVisible}
+                      class="text-center text-black dark:text-white w-full items-center"
+                    >
+                      <div class="mt-4">
+                        <label class="block mb-2 text-m font-medium text-left text-white dark:text-white">
+                          Amount
+                        </label>
+                        <input
+                          required
+                          type="number"
+                          placeholder="2990"
+                          className="input input-bordered w-full"
+                          onChange={(e) => {
+                            setAmount(parseInt(e.target.value));
+                          }}
+                        />
+                      </div>
+                      <div class="mt-4">
+                        <label class="block mb-2 text-m font-medium text-left text-white dark:text-white">
+                          Date
+                        </label>
+                        <input
+                          required
+                          type="date"
+                          placeholder="12/12/2023"
+                          className="input input-bordered w-full"
+                          onChange={(e) => {
+                            setDate(e.target.value);
+                          }}
+                        />
+                      </div>
+                      <div class="mt-4">
+                        <label class="block mb-2 text-m font-medium text-left text-white dark:text-white">
+                          Description
+                        </label>
+                        <input
+                          required
+                          type="text"
+                          placeholder="Ticket to Tokyo"
+                          className="input input-bordered w-full"
+                          onChange={(e) => {
+                            setDesc(e.target.value);
+                          }}
+                        />
+                      </div>
+                      <div class="flex justify-center items-center mt-4">
+                        <input
+                          type="radio"
+                          id="expense"
+                          name="type"
+                          value="EXPENSE"
+                          className="radio radio-error mx-2"
+                          checked={type === "EXPENSE"}
+                          onChange={(e) => setType(e.target.value)}
+                        />
+                        <label htmlFor="expense">Expense</label>
+                        <input
+                          type="radio"
+                          id="income"
+                          name="type"
+                          value="INCOME"
+                          className="radio radio-success mx-2"
+                          checked={type === "INCOME"}
+                          onChange={(e) => setType(e.target.value)}
+                        />
+                        <label htmlFor="Expense">Income</label>
+                      </div>
+                      <button
+                        class="btn btn-primary mt-4"
+                        onClick={() => {
+                          if (
+                            (desc.length > 0 &&
+                              date.length > 0 &&
+                              type === "EXPENSE") ||
+                            type === "INCOME"
+                          ) {
+                            setData([...data, { amount, desc, date, type }]);
+                          }
+                        }}
+                      >
+                        ADD TRANSACTION
+                      </button>
+                    </div>
+                  )}
                   <div
                     className="font-extrabold mt-4 border-b-2 mb-2 pb-2 border-indigo-600 flex flex-wrap justify-between items-center w-full
 "
@@ -43,13 +169,13 @@ const Expense = () => {
 "
                   >
                     <p class=" text-xl font-semibold text-emerald-400">
-                      INCOME HERE
+                      ${income}
                     </p>
                     <p class=" text-xl font-semibold text-red-400">
-                      EXPENSE HERE
+                      ${expense}
                     </p>
                   </div>
-                  <div class="flex justify-between items-center mb- mt-2">
+                  <div class="flex justify-between items-center mt-2">
                     <h3 class="text-xl font-bold leading-none text-gray-900 dark:text-white">
                       History
                     </h3>
@@ -59,21 +185,20 @@ const Expense = () => {
                     placeholder="Search..."
                     class="input input-bordered input-sm w-full mt-4"
                   />
-                  <ExpenseCard />
-                  {/* {data?.map((expense, index) => {
-                    return (
-                      <ExpenseCard
-                        expenseNameProps={expense.expenseName}
-                        dateProps={expense.date}
-                        totalMoneyProps={expense.totalMoney}
-                        transactionProps={expense.transaction}
-                        index={index}
-                        expense={expense}
-                        removeExpense={removeExpense}
-                        balance={balance}
-                      />
-                    );
-                  })} */}
+                  <div>
+                    {data?.map((payload, index) => {
+                      return (
+                        <ExpenseCard
+                          descProps={payload.desc}
+                          amountProps={payload.amount}
+                          dateProps={payload.date}
+                          typeProps={payload.type}
+                          index={index}
+                          payload={payload}
+                        />
+                      );
+                    })}
+                  </div>
                 </div>
               </div>
             </div>
--- src/pages/ExpenseCard.jsx
+++ src/pages/ExpenseCard.jsx
@@ -1,27 +1,36 @@
-import React from "react";
+import { useEffect, useState, React } from "react";
 
 const ExpenseCard = ({
-  balance,
-  transactionProps,
+  descProps,
   dateProps,
-  expenseNameProps,
+  amountProps,
+  typeProps,
+  payload,
+  sign,
+  props,
 }) => {
   return (
     <div>
-      <div class="flow-root">
+      <div class="flow-root mt-2">
         <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
           <li class="py-3">
             <p class="text-s font-extrabold text-gray-900 truncate dark:text-white">
-              TEXT
+              {descProps}
             </p>
-            <div class="flex items-center space-x-4 border-purple-600 border-b-2 mb-2 pb-2 mt-1">
+
+            <div class="flex items-center space-x-4 border-white border-b-2">
               <div class="flex-1 min-w-0">
                 <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                  DATE
+                  {dateProps}
                 </p>
               </div>
-              <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                MONEYMONYE
+              <div
+                style={{
+                  color: payload.type === "EXPENSE" ? "#f87171" : "#34d399",
+                }}
+                class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white"
+              >
+                ${amountProps}
               </div>
             </div>
           </li>
