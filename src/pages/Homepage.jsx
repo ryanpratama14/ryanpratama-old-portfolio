458d6924a694f61df7627e6aee71463f80dec8a4
Ryan Pratama <ru.ryanpratama@gmail.com>
2022-12-31 15:56:42 +0300

    design

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,7 +6,9 @@ const Homepage = () => {
       <section>
         <div class="mx-auto max-w-screen-xl px-8 py-20 sm:px-6 sm:pb-24 lg:px-8">
           <div class="mx-auto max-w-xl text-center">
-            <h2 class="text-4xl font-bold tracking-tight sm:text-5xl">HI!</h2>
+            <h2 class="text-4xl font-bold tracking-tight text-base-content sm:text-5xl">
+              HI!
+            </h2>
           </div>
         </div>
       </section>
