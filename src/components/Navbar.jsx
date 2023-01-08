8ce71d930851dac8384a89dead587fd097ea9bd6
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-01-08 05:40:02 +0300

    adjusted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,11 +18,11 @@ const Navbar = () => {
   return (
     <div
       class="
-    sticky top-0 z-30  h-16 bg-opacity-90 backdrop-blur duration-100 
+    sticky top-0 z-30  bg-opacity-90 backdrop-blur duration-100 
     text-primary-content shadow
     "
     >
-      <div class="navbar flex justify-center">
+      <div class="navbar flex justify-center items-center">
         <div class="navbar-start">
           <div class="dropdown">
             <label tabindex="0" class="btn btn-ghost btn-circle">
@@ -80,10 +80,13 @@ const Navbar = () => {
             Homepage
           </NavLink>
         </div>
-        <div class="navbar-end ">
+        <div class="navbar-end">
           <ThemeChanger />
-          <div class="dropdown dropdown-end ml-0.5">
-            <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+          <div class="dropdown dropdown-end ml-1">
+            <label
+              tabindex="0"
+              class="btn btn-ghost btn-circle avatar flex items-center"
+            >
               <div class="w-16 rounded-full">
                 <img
                   alt="ryan display profile"
--- src/pages/Home/Homepage.jsx
+++ src/pages/Home/Homepage.jsx
@@ -4,7 +4,7 @@ const Homepage = () => {
   return (
     <section
       id="homepage"
-      class="w-full px-[8vw] md:px-[4vw] -mt-[4rem] pt-[4rem] pb-[20rem] from-primary via-secondary to-accent bg-gradient-to-b"
+      class="w-full px-[8vw] md:px-[4vw] -mt-[5rem] pt-[4rem] pb-[20rem] from-primary via-secondary to-accent bg-gradient-to-b"
     >
       <h1 class="text-5xl font-bold text-center py-20 text-primary-content">
         HI
